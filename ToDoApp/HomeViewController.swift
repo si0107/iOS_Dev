1100070997f78f60b2970fbda25fe24bbf756a41
si0107 <sarikaislam.02@gmail.com>
2022-11-15 02:56:19 -0500

    obtain location for weather api

--- ToDoApp/HomeViewController.swift
+++ ToDoApp/HomeViewController.swift
@@ -8,8 +8,9 @@
 import UIKit
 
 import FSCalendar
+import CoreLocation
 
-class HomeViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource {
+class HomeViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSource, CLLocationManagerDelegate {
     
     @IBOutlet weak var yearLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
@@ -17,8 +18,13 @@ class HomeViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSo
     @IBOutlet var temperature: UILabel!
     //@IBOutlet weak var quote: UILabel!
     @IBOutlet var calendar: FSCalendar!
-    var formatter = DateFormatter()
+    @IBOutlet var cityLabel: UILabel!
     
+    var formatter = DateFormatter()
+    let locationManager = CLLocationManager()
+    var location: CLLocation?
+    var updatingLocation = false
+    var lastLocationError: Error?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,10 +50,13 @@ class HomeViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSo
         
         //self.quote.text = "Hello my name is sarika islam. Hello my name is sarika islam. Hello my name is sarika islam. Hello my name is sarika islam. Hello my name is sarika islam. Hello my name is sarika islam. Hello my name is sarika islam. Hello my name is sarika islam. "
         
+        getLocation()
+        //TODO: updateCity()
+        
     }
     
     
-    //MARK:- Calendar Delegate
+    //MARK: - Calendar Delegate
     func calendar(_ calendar: FSCalendar, didSelect date: Date, at monthPosition: FSCalendarMonthPosition) {
         
         formatter.dateFormat = "dd-MM-yyyy"
@@ -56,7 +65,75 @@ class HomeViewController: UIViewController, FSCalendarDelegate, FSCalendarDataSo
     }
     
     
-
+    // MARK: - Location Helper functions
+    func getLocation() {
+        //check authorization status
+        let authStatus = locationManager.authorizationStatus
+        if authStatus == .notDetermined {
+            locationManager.requestWhenInUseAuthorization()
+            return
+        }
+        
+        if authStatus == .denied || authStatus == .restricted {
+            showLocationServicesDeniedAlert()
+            return
+        }
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.startUpdatingLocation()
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    func locationManager(
+      _ manager: CLLocationManager,
+      didFailWithError error: Error
+    ){
+        print("didFailWithError \(error.localizedDescription)")
+        if (error as NSError).code == CLError.locationUnknown.rawValue{
+            return
+        }
+        lastLocationError = error
+        stopLocationManager()
+        //TODO: updateCity()
+    }
+    
+    func locationManager(
+      _ manager: CLLocationManager,
+      didUpdateLocations locations: [CLLocation]
+    ){
+        let newLocation = locations.last!
+        print("didUpdateLocations \(newLocation)")
+        location = newLocation    // Add this
+        //TODO: update city location here
+        //TODO: updateCity()
+    }
+    
+    // MARK: - Helper Methods
+    func showLocationServicesDeniedAlert() {
+        let alert = UIAlertController(
+            title: "Location Services Disabled",
+            message: "Please enable location services for this app in Settings.",
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: .default,
+            handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    //TODO: func updateCity(){cityLabel.text = newCity}
+    
+    func stopLocationManager() {
+        if updatingLocation {
+            locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            updatingLocation = false
+        }
+    }
+    
+    
 
 }
 
